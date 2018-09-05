0364347c1a4d4c6417601f4ae52eadef96086f05
James Reynolds <toast@drtoast.com>
2018-09-05 16:08:16 -0700

    Add sk devtools

--- bash/paths.sh
+++ bash/paths.sh
@@ -4,11 +4,12 @@ export PATH="/usr/local/bin:$PATH"
 # Me
 export PATH="$HOME/bin:$PATH"
 
-# jRuby
-# export PATH="$JAVA_HOME/bin:$PATH"
-
 # Node
 export NODE_PATH="/usr/local/lib/node_modules"
 
 # Heroku
 export PATH="/usr/local/heroku/bin:$PATH"
+
+# SK Devtools
+export PATH="$HOME/devtools:$PATH"
+  
