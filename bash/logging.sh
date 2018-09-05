5957c48b34e4de763ea97dd87a354f3cefe7b7de
James Reynolds <toast@drtoast.com>
2018-09-05 16:08:06 -0700

    Remove logging

--- bash/logging.sh
+++ bash/logging.sh
@@ -1,2 +1,2 @@
 export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log; fi'
-export PROMPT_COMMAND="$PROMPT_COMMAND; update_terminal_cwd;"
+#export PROMPT_COMMAND="$PROMPT_COMMAND; update_terminal_cwd;"
